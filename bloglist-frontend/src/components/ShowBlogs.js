86d0e52a60b73b161f30fa249dce91f0a8ecd0b5
Tino <tino.teittinen@edu.turkuamk.fi>
2023-01-07 21:26:16 +0200

    exercise 5.9 added

--- bloglist-frontend/src/components/ShowBlogs.js
+++ bloglist-frontend/src/components/ShowBlogs.js
@@ -5,7 +5,6 @@ const ShowBlogs = ({
   createBlog, Togglable,
   handleLike 
   }) => {
-
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 5,
@@ -13,13 +12,16 @@ const ShowBlogs = ({
     borderWidth: 1,
     marginBottom: 5
   }
+
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
+  
     return (
       <div>
       <h2>blogs</h2>
       <p>Currenty logged in as {user.name}: <Button handler={handleLogout} text='logout'/></p>
       <p>All blogs:</p>
         <ul>
-          {blogs.map( blog =>
+          {sortedBlogs.map( blog =>
             <li key={blog.id} style={blogStyle}>
               <div>
               <Togglable buttonLabel='view' title={blog.title}>
